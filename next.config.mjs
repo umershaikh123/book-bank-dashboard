f4bb321940ee22efcf1cd8753463ade99008af67
umershaikh123 <umershaikh217@gmail.com>
2025-01-07 23:20:42 +0500

    remove : eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-}
+const nextConfig = {}
 
 export default nextConfig
