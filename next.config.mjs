c7fb784cdad7b2bd740b4fd5d6ca265351ba4ffe
umershaikh123 <umershaikh217@gmail.com>
2025-01-13 19:12:33 +0500

    deployment fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  missingSuspenseWithCSRBailout: false,
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   async headers() {
     return [
       {
